a3991cd8429158a2d495898c23d127bc6d2d2a5e
annafy149 <113755549+annafy149@users.noreply.github.com>
2023-05-01 21:48:49 -0400

    Updating the database credentials.

--- flutter-app/firebase_options.dart
+++ flutter-app/firebase_options.dart
@@ -1,12 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 
 final FirebaseOptions firebaseOptions = FirebaseOptions(
-  apiKey: "AIzaSyANO-45Sq1N09OjxfPKESvdll1bE287f_g",
-  authDomain: "ewb-voltage-remote-sensing.firebaseapp.com",
-  databaseURL: "https://ewb-voltage-remote-sensing-default-rtdb.firebaseio.com/",
-  projectId: "ewb-voltage-remote-sensing",
-  storageBucket: "ewb-voltage-remote-sensing.appspot.com",
-  messagingSenderId: "339223991819",
-  appId: "1:339223991819:web:6d28e1e74e947bfb82990e",
-  measurementId: "G-YX44K8X8BL"
+  apiKey: "########################################",
+  authDomain: "sentinelai.firebaseapp.com",
+  databaseURL: "sentinelai.firebaseio.com/",
+  projectId: "sentinelai",
+  storageBucket: "sentinelai.appspot.com",
+  messagingSenderId: "############",
+  appId: "1:#######################################",
+  measurementId: "G-##########"
 );
